78cde72bf3a6402cf941f0285e56380723321a53
Fedotos228 <tudor.onceanu@gmail.com>
2026-07-10 14:58:27 +0300

    fix: type annotation for serverFunction in layout.tsx

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,6 +1,7 @@
 import config from '@payload-config'
 import '@payloadcms/next/css'
 import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
+import type { ServerFunctionClient } from 'payload'
 import React from 'react'
 
 import { importMap } from './admin/importMap.js'
@@ -9,7 +10,7 @@ type Args = {
   children: React.ReactNode
 }
 
-const serverFunction = async (args: Parameters<typeof handleServerFunctions>[0]) => {
+const serverFunction: ServerFunctionClient = async (args) => {
   'use server'
   return handleServerFunctions({ ...args, config, importMap })
 }
