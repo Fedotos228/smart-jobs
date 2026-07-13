7ab4da117a4e529057720d6d48aed99344c7ccc5
Fedotos228 <tudor.onceanu@gmail.com>
2026-07-13 10:55:43 +0300

    feat: add remote pattern for Vercel Blob Storage in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://novalines-cdn.stellarsolutions.md/smartjobs/**')]
+    remotePatterns: [
+      new URL('https://novalines-cdn.stellarsolutions.md/smartjobs/**'),
+      new URL('https://*.public.blob.vercel-storage.com/**'),
+    ]
   },
   turbopack: {
     resolveAlias: {
